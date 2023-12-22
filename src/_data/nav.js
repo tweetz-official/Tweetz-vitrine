0323b1cbe978dc7255aab796e9cfbc68aaadca3f
Romain <git@romainsavigny.be>
2023-12-22 14:34:04 +0100

    UPDATE: ajout page creator

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -1,7 +1,4 @@
 module.exports = [
-  {
-    navLabel: "Home",
-    navTrigger: "home",
-    navUrl: "/",
-  }
+    { navLabel: "Home", navTrigger: "home", navUrl: "/" },
+    { navLabel: "Creator", navTrigger: "creator", navUrl: "/creator/" },
 ];
