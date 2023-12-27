240c2fcc9363817c5e13986690c6898f8898bdb6
Romain <git@romainsavigny.be>
2023-12-27 13:50:27 +0100

    UPDATE: Add page beta setup

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -1,4 +1,5 @@
 module.exports = [
     { navLabel: "Home", navTrigger: "home", navUrl: "/" },
     { navLabel: "Creator", navTrigger: "creator", navUrl: "/creator/" },
+    { navLabel: "Beta", navTrigger: "beta", navUrl: "/beta/" },
 ];
