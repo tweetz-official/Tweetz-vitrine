1989240fc6319955dec958ff5502038fa95f8f98
Valentin de Brabandère <valentindebrabandere@gmail.com>
2024-01-15 14:38:02 +0100

    NEW: BAsi gallery-Implementation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,11 @@
 import * as navigation from "./modules/navigation.js";
 import * as hero from "./modules/hero.js";
+import  * as gallery from "./modules/gallery-init.js";
+import * as scroll from "./modules/scrollEffect.js"; 
 
+document.body.classList.add("js-enabled");
+
+gallery.init();
+scroll.init();
 navigation.init();
 hero.init();
